66aa6c9b655d68d13331047e8491fb76ebe5c271
yuu.konno <yuu.konno@ateliers.dev>
2020-06-30 07:31:20 +0900

    StringExtensionsを追加

---
+++ Ateliers.Core.UnitTest/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Ateliers.UnitTest.Extensions
+{
+    public class StringExtensionsTest
+    {
+        public const string TESTNAME_001_00100 = nameof(StringExtensions) + "." + nameof(StringExtensions.Sanitize) + "_" + "NullがEmptyに置き換えられる";
+        public const string TESTNAME_001_00200 = nameof(StringExtensions) + "." + nameof(StringExtensions.Sanitize) + "_" + "Null以外の場合はそのままの文字列が返る";
+        public const string TESTNAME_001_00300 = nameof(StringExtensions) + "." + nameof(StringExtensions.Sanitize) + "_" + "Emptyでも正常終了する";
+
+        public const string TESTNAME_002_00100 = nameof(StringExtensions) + "." + nameof(StringExtensions.IsNullOrWhiteSpace) + "_" + "Nullと空白はtrueが返る";
+        public const string TESTNAME_002_00200 = nameof(StringExtensions) + "." + nameof(StringExtensions.IsNullOrWhiteSpace) + "_" + "Nullと空白以外はfalseが返る";
+
+        public const string TESTNAME_003_00100 = nameof(StringExtensions) + "." + nameof(StringExtensions.SubstringLeft) + "_" + "正しく文字列を切り取って抽出できる";
+        public const string TESTNAME_003_00200 = nameof(StringExtensions) + "." + nameof(StringExtensions.SubstringLeft) + "_" + "対象文字列が存在しない場合はそのままの文字列が返る";
+        public const string TESTNAME_003_00300 = nameof(StringExtensions) + "." + nameof(StringExtensions.SubstringLeft) + "_" + "検索キーの文字列がNullの場合は例外エラー";
+        public const string TESTNAME_003_00400 = nameof(StringExtensions) + "." + nameof(StringExtensions.SubstringLeft) + "_" + "検索キーの文字列がEmptyの場合は例外エラー";
+
+        [Fact(DisplayName = TESTNAME_001_00100)]
+        public void TEST_001_00100()
+        {
+            var assumedResult = string.Empty;
+            var testResult = default(string).Sanitize();
+
+            testResult.Is(assumedResult);
+        }
+
+        [Fact(DisplayName = TESTNAME_001_00200)]
+        public void TEST_001_00200()
+        {
+            var assumedResult = "TestString";
+            var testResult = "TestString".Sanitize();
+
+            testResult.Is(assumedResult);
+        }
+
+        [Fact(DisplayName = TESTNAME_001_00300)]
+        public void TEST_001_00300()
+        {
+            var assumedResult = string.Empty;
+            var testResult = string.Empty.Sanitize();
+
+            testResult.Is(assumedResult);
+        }
+
+        [Fact(DisplayName = TESTNAME_002_00100)]
+        public void TEST_002_00100()
+        {
+            foreach (var testStr in new[] { default, string.Empty, " " })
+            {
+                testStr.IsNullOrWhiteSpace().IsTrue();
+            }
+        }
+
+        [Fact(DisplayName = TESTNAME_002_00200)]
+        public void TEST_002_00200()
+        {
+            foreach (var testStr in new[] { "AAA", "あああ", "111", "１１１" })
+            {
+                testStr.IsNullOrWhiteSpace().IsFalse();
+            }
+        }
+
+        [Fact(DisplayName = TESTNAME_003_00100)]
+        public void TEST_003_00100()
+        {
+            var testDatas = new List<SubstringLeftTestModel>
+            {
+                new SubstringLeftTestModel { TestString = "TestString", KeyString = "Test", AssumedString = "String" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "AB", AssumedString = "BCC" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "BB", AssumedString = "CC" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "CC", AssumedString = string.Empty },
+                new SubstringLeftTestModel { TestString = "AA BB CC", KeyString = " ", AssumedString = "BB CC" },
+            };
+
+            foreach (var testData in testDatas)
+            {
+                var testResult = testData.TestString.SubstringLeft(testData.KeyString);
+                testResult.Is(testData.AssumedString);
+            }
+        }
+
+        [Fact(DisplayName = TESTNAME_003_00200)]
+        public void TEST_003_00200()
+        {
+            var testDatas = new List<SubstringLeftTestModel>
+            {
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "Z", AssumedString = "AABBCC" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "AAA", AssumedString = "AABBCC" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "BBB", AssumedString = "AABBCC" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = "CCC", AssumedString = "AABBCC" },
+                new SubstringLeftTestModel { TestString = "AABBCC", KeyString = " ", AssumedString = "AABBCC" },
+            };
+
+            foreach (var testData in testDatas)
+            {
+                var testResult = testData.TestString.SubstringLeft(testData.KeyString);
+                testResult.Is(testData.AssumedString);
+            }
+        }
+
+        [Fact(DisplayName = TESTNAME_003_00300)]
+        public void TEST_003_00300()
+        {
+            Assert.ThrowsAny<ArgumentNullException>(() => "A".SubstringLeft(default(string))).Message.Contains(StringExtensions.ExceptionMessage100100);
+        }
+
+        [Fact(DisplayName = TESTNAME_003_00400)]
+        public void TEST_003_00400()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => "A".SubstringLeft(string.Empty)).Message.Contains(StringExtensions.ExceptionMessage100101);
+        }
+
+        /// <summary>
+        /// <see cref="StringExtensions.SubstringLeft(string, string)"/> テスト用データモデル
+        /// </summary>
+        private class SubstringLeftTestModel
+        {
+            /// <summary>
+            /// 想定する結果の文字列 
+            /// </summary>
+            public string AssumedString { get; set; }
+
+            /// <summary>
+            /// テストする文字列
+            /// </summary>
+            public string TestString { get; set; }
+            
+            /// <summary>
+            /// 検索キーとなる文字列
+            /// </summary>
+            public string KeyString { get; set; }
+        }
+    }
+}
---
+++ Ateliers.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ateliers
+{
+    /// <summary>
+    /// 文字列型(<see cref="string"/>) 拡張メソッド
+    /// </summary>
+    public static class StringExtensions
+    {
+        /*--- Property/Field Definitions ----------------------------------------------------------------------------------------------------------*/
+
+        #region --- 例外メッセージ ---------
+
+        /// <summary>
+        /// <see cref="SubstringLeft(string, string)"/> の検索する文字列がNULL指定された場合の例外メッセージを取得します。
+        /// </summary>
+        public static string ExceptionMessage100100 { get; } = $"検索キーとする文字列の指定は必須です。";
+
+        /// <summary>
+        /// <see cref="SubstringLeft(string, string)"/> の検索する文字列が <see cref="string.Empty"/> に指定された場合の例外メッセージを取得します。
+        /// </summary>
+        public static string ExceptionMessage100101 { get; } = $"検索キーする文字列を {nameof(string.Empty)} に指定することはできません。";
+
+        #endregion
+
+        /*--- Method: internal --------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary>
+        /// 指定された文字列を左側から検索し、最初に一致する対象文字列より左側を取り除いた文字列を返却します。
+        /// </summary>
+        /// <param name="self"> 取り除きの対象となる文字列(自身) </param>
+        /// <param name="keyStr"> 検索する(取り除く)文字列を指定します。 </param>
+        /// <returns> 対象文字列より左側を切り取った文字列を返します。対象が見つからない場合は、そのままの文字列を返します。 </returns>
+        /// <exception cref="ArgumentNullException"> 取り除く文字列を Null にすることはできません。 </exception>
+        /// <exception cref="ArgumentException"> 取り除く文字列を <see cref="string.Empty"/> に指定することはできません。 </exception>
+        /// <example>
+        /// 構文: "AAABBBCCC".SubstringLeft("A")            結果: "AABBBCCC"
+        /// 構文: "AAABBBCCC".SubstringLeft("BB")           結果: "BCCC"
+        /// 構文: "AAABBBCCC".SubstringLeft("BC")           結果: "CC"
+        /// 構文: "TESTRESULT_xxxx".SubstringLeft("_")      結果: "xxxx"
+        /// 構文: "TESTRESULT_xxxx_yyy".SubstringLeft("_")  結果: "xxxx_yyy"
+        /// 構文: "2020/06/29".SubstringLeft("/")           結果: "06/29"
+        /// 構文: "2020/06/29".SubstringLeft("A")           結果: "2020/06/29"
+        /// </example>
+        public static string SubstringLeft(this string self, string keyStr)
+        {
+            if (keyStr == default)
+                throw new ArgumentNullException(nameof(keyStr), ExceptionMessage100100);
+            else if (keyStr == string.Empty)
+                throw new ArgumentException(ExceptionMessage100101, nameof(keyStr));
+
+            if (!self.Contains(keyStr))
+                return self;
+ 
+            return self.Substring(self.IndexOf(keyStr) + keyStr.Length);
+        }
+
+        /// <summary>
+        /// 文字列がNullまたは空白("", " ")であるかを確認し、結果を返却します。
+        /// </summary>
+        /// <param name="src"> 確認する文字列(自身) </param>
+        /// <returns> Null または 空白 である場合は true, そうでない場合は false を返します。 </returns>
+        public static bool IsNullOrWhiteSpace(this string src) => string.IsNullOrWhiteSpace(src);
+
+        /// <summary>
+        /// 文字列が Null の場合、空白に置き換えて返却します。
+        /// </summary>
+        /// <param name="src"> 確認する文字列(自身) </param>
+        /// <returns> 対象が Null の場合は <see cref="string.Empty"/>, そうでない場合はそのままの文字列を返します。 </returns>
+        public static string Sanitize(this string src) => src ?? string.Empty;
+
+        /*--- Method: private ---------------------------------------------------------------------------------------------------------------------*/
+
+    }
+}
