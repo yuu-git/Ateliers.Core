b62f5e0a5d68df1e0b3424cdb1f86d55ee27c8a2
yuu.konno <yuu.konno@ateliers.dev>
2020-07-01 00:31:32 +0900

    IEnumerable 拡張メソッド 追加

---
+++ Ateliers.Core.UnitTest/Extensions/IEnumerableExtensionsTest.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Ateliers.UnitTest.Extensions
+{
+    public class IEnumerableExtensionsTest
+    {
+        public const string TESTNAME_001_00100 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.IsDefaultOrEmpty) + "_" + "インスタンスがNullの時はtrue";
+        public const string TESTNAME_001_00200 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.IsDefaultOrEmpty) + "_" + "インスタンスのコレクションが0件の時はtrue";
+        public const string TESTNAME_001_00300 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.IsDefaultOrEmpty) + "_" + "インスタンスのコレクションにデータがある時はfalse";
+
+        public const string TESTNAME_002_00100 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.IsEmpty) + "_" + "インスタンスのコレクションが0件の時はtrue";
+        public const string TESTNAME_002_00200 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.IsEmpty) + "_" + "インスタンスのコレクションにデータがある時はfalse";
+        public const string TESTNAME_002_00300 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.IsEmpty) + "_" + "インスタンスがNullの時は例外エラー";
+
+        public const string TESTNAME_003_00100 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.Sanitize) + "_" + "インスタンスがNullの時はインスタンスが生成される";
+        public const string TESTNAME_003_00200 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.Sanitize) + "_" + "インスタンスがNull以外の時はそのまま返る";
+
+        public const string TESTNAME_004_00100 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.ToAllUpper) + "_" + "文字列が全て大文字に変換される";
+        public const string TESTNAME_004_00200 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.ToAllUpper) + "_" + "インスタンスのコレクションがカラでも正常終了する";
+        public const string TESTNAME_004_00300 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.ToAllUpper) + "_" + "インスタンスのコレクションがNullの時は例外エラー";
+
+        public const string TESTNAME_005_00100 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.ToAllLower) + "_" + "文字列が全て小文字に変換される";
+        public const string TESTNAME_005_00200 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.ToAllLower) + "_" + "インスタンスのコレクションがカラでも正常終了する";
+        public const string TESTNAME_005_00300 = nameof(IEnumerableExtensions) + "." + nameof(IEnumerableExtensions.ToAllLower) + "_" + "インスタンスのコレクションがNullの時は例外エラー";
+
+        [Fact(DisplayName = TESTNAME_001_00100)]
+        public void TEST_001_00100()
+        {
+            default(List<string>).IsDefaultOrEmpty().IsTrue();
+        }
+
+        [Fact(DisplayName = TESTNAME_001_00200)]
+        public void TEST_001_00200()
+        {
+            new List<string>().IsDefaultOrEmpty().IsTrue();
+        }
+
+        [Fact(DisplayName = TESTNAME_001_00300)]
+        public void TEST_001_00300()
+        {
+            new List<string> { "TEST" }.IsDefaultOrEmpty().IsFalse();
+        }
+
+        [Fact(DisplayName = TESTNAME_002_00100)]
+        public void TEST_002_00100()
+        {
+            new List<string>().IsEmpty().IsTrue();
+        }
+
+        [Fact(DisplayName = TESTNAME_002_00200)]
+        public void TEST_002_00200()
+        {
+            new List<string> { "TEST" }.IsEmpty().IsFalse();
+        }
+
+        [Fact(DisplayName = TESTNAME_002_00300)]
+        public void TEST_002_00300()
+        {
+            Assert.ThrowsAny<NullReferenceException>(() => default(List<string>).IsEmpty()).Message.Contains(IEnumerableExtensions.ExceptionMessage100100);
+        }
+
+        [Fact(DisplayName = TESTNAME_003_00100)]
+        public void TEST_003_00100()
+        {
+            var testResult = default(List<string>).Sanitize();
+
+            testResult.IsNotNull();
+            testResult.IsEmpty().IsTrue();
+        }
+
+        [Fact(DisplayName = TESTNAME_003_00200)]
+        public void TEST_003_00200()
+        {
+            var testResult = new List<string>().Sanitize();
+
+            testResult.IsNotNull();
+            testResult.IsEmpty().IsTrue();
+
+            testResult = new List<string> { "A" }.Sanitize();
+
+            testResult.IsNotNull();
+            testResult.IsEmpty().IsFalse();
+            testResult.Contains("A").IsTrue();
+        }
+
+        [Fact(DisplayName = TESTNAME_004_00100)]
+        public void TEST_004_00100()
+        {
+            var testDatas = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+                new Tuple<string, string>("aAbBcC", "AABBCC"),
+                new Tuple<string, string>("a b_c", "A B_C"),
+                new Tuple<string, string>("oo00", "OO00"),
+                new Tuple<string, string>("TEST", "TEST"),
+            };
+
+            var testResult = testDatas.Select(x => x.Item1).ToAllUpper().ToList();
+
+            for (int i = 0; i < testDatas.Count(); i++)
+            {
+                testResult[i].Is(testDatas[i].Item2);
+            }
+        }
+
+        [Fact(DisplayName = TESTNAME_004_00200)]
+        public void TEST_004_00200()
+        {
+            new List<string>().ToAllUpper();
+        }
+
+        [Fact(DisplayName = TESTNAME_004_00300)]
+        public void TEST_004_00300()
+        {
+            Assert.ThrowsAny<NullReferenceException>(() => default(List<string>).ToAllUpper()).Message.Contains(IEnumerableExtensions.ExceptionMessage100200).IsTrue();
+        }
+
+        [Fact(DisplayName = TESTNAME_004_00100)]
+        public void TEST_005_00100()
+        {
+            var testDatas = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"),
+                new Tuple<string, string>("aAbBcC", "aabbcc"),
+                new Tuple<string, string>("A B_C", "a b_c"),
+                new Tuple<string, string>("OO00", "oo00"),
+                new Tuple<string, string>("TEST", "test"),
+            };
+
+            var testResult = testDatas.Select(x => x.Item1).ToAllLower().ToList();
+
+            for (int i = 0; i < testDatas.Count(); i++)
+            {
+                testResult[i].Is(testDatas[i].Item2);
+            }
+        }
+
+        [Fact(DisplayName = TESTNAME_004_00200)]
+        public void TEST_005_00200()
+        {
+            new List<string>().ToAllLower();
+        }
+
+        [Fact(DisplayName = TESTNAME_004_00300)]
+        public void TEST_005_00300()
+        {
+            Assert.ThrowsAny<NullReferenceException>(() => default(List<string>).ToAllLower()).Message.Contains(IEnumerableExtensions.ExceptionMessage100300).IsTrue();
+        }
+    }
+}
---
+++ Ateliers.Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ateliers
+{
+    /// <summary>
+    /// <see cref="IEnumerable{T}"/> 拡張メソッド
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /*--- Property/Field Definitions ----------------------------------------------------------------------------------------------------------*/
+
+        #region --- 例外メッセージ ---------
+
+        /// <summary>
+        /// <see cref="IsEmpty{T}(IEnumerable{T})"/> の検索する文字列がNULL指定された場合の例外メッセージを取得します。
+        /// </summary>
+        public static string ExceptionMessage100100 { get; } = $"カラを確認するコレクションのインスタンスがNullです。";
+
+        /// <summary>
+        /// <see cref="ToAllUpper(IEnumerable{string})"/> の確認するコレクションがNullに指定された場合の例外メッセージを取得します。
+        /// </summary>
+        public static string ExceptionMessage100200 { get; } = $"文字列を変換するコレクションのインスタンスがNullです。";
+
+        /// <summary>
+        /// <see cref="ToAllLower(IEnumerable{string})"/> の確認するコレクションがNullに指定された場合の例外メッセージを取得します。
+        /// </summary>
+        public static string ExceptionMessage100300 { get; } = $"文字列を変換するコレクションのインスタンスがNullです。";
+
+        #endregion
+
+        /*--- Method: internal --------------------------------------------------------------------------------------------------------------------*/
+
+        /// <summary>
+        /// コレクションがカラであるかを確認します。
+        /// </summary>
+        /// <typeparam name="T"> 確認するコレクションの型を指定します。 </typeparam>
+        /// <param name="self"> 確認するコレクション（自身） </param>
+        /// <returns> コレクションがカラであった場合は true, そうでなかった場合は false を返します。 </returns>
+        /// <exception cref="NullReferenceException"> 確認するコレクションのインスタンスは生成されている必要があります。 </exception>
+        public static bool IsEmpty<T>(this IEnumerable<T> self)
+        {
+            if (self == default) 
+                throw new NullReferenceException(ExceptionMessage100100);
+
+            return !self.Any();
+        }
+
+        /// <summary>
+        /// コレクションが既定値またはカラであるかを確認します。
+        /// </summary>
+        /// <typeparam name="T"> 確認するコレクションの型を指定します。 </typeparam>
+        /// <param name="self"> 確認するコレクション（自身） </param>
+        /// <returns> コレクションが既定値またはカラであった場合は true, そうでなかった場合は false を返します。 </returns>
+        public static bool IsDefaultOrEmpty<T>(this IEnumerable<T> self)
+        {
+            return self == default || self.IsEmpty();
+        }
+
+        /// <summary>
+        /// コレクションが規定値の場合、新しいコレクションを生成して返却します。
+        /// </summary>
+        /// <typeparam name="T"> コレクションに格納する型を指定します。 </typeparam>
+        /// <param name="self"> 確認するコレクションを指定します。(自身) </param>
+        /// <returns> 規定値であった場合は新しいコレクション、それ以外の場合は引数のコレクションをそのまま返します。 </returns>
+        public static IEnumerable<T> Sanitize<T>(this IEnumerable<T> self)
+        {
+            return self ?? new List<T>();
+        }
+
+        /// <summary>
+        /// コレクション内の文字列を全て大文字に変換します。
+        /// </summary>
+        /// <param name="self"> 対象の文字列コレクション(自身) </param>
+        /// <returns> 文字列を大文字に変換した新しいリストを返します。 </returns>
+        /// <exception cref="NullReferenceException"> 返還対象を格納しているリストを Null にすることはできません。 </exception>
+        public static IEnumerable<string> ToAllUpper(this IEnumerable<string> self)
+        {
+            if (self == default)
+                throw new NullReferenceException(ExceptionMessage100200);
+
+            return self.Select(x => x.ToUpper()).ToList();
+        }
+
+        /// <summary>
+        /// コレクション内の文字列を全て小文字に変換します。
+        /// </summary>
+        /// <param name="self"> 対象の文字列コレクション(自身) </param>
+        /// <returns> 文字列を小文字に変換した新しいリストを返します。 </returns>
+        /// <exception cref="NullReferenceException"> 返還対象を格納しているリストを Null にすることはできません。 </exception>
+        public static IEnumerable<string> ToAllLower(this IEnumerable<string> self)
+        {
+            if (self == default)
+                throw new NullReferenceException(ExceptionMessage100300);
+
+            return self.Select(x => x.ToLower()).ToList();
+        }
+    }
+}
