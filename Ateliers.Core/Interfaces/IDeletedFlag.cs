9a899cb504a71e5b7db5d3725cb0ca0c5ee9a1c3
yuu.konno <yuu.konno@ateliers.dev>
2024-03-02 03:32:25 +0900

    エンティティの 登録, 更新, 削除 情報プロパティ実装用のインタフェースを追加

---
+++ Ateliers.Core/Interfaces/IDeletedFlag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ateliers
+{
+    /// <summary>
+    /// 論理削除フラグ
+    /// </summary>
+    /// <remarks>
+    /// <para> 概要: エンティティが論理削除されているかを判定できるフラグを保持していることを示す。 </para>
+    /// </remarks>
+    public interface IDeletedFlag
+    {
+        /*--- Property/Field Definitions ----------------------------------------------------------------------------------------------------------*/
+
+        /// <summary>
+        /// 論理削除フラグを取得します。
+        /// </summary>
+        bool IsDeleted { get; }
+
+        /*--- Method: public ----------------------------------------------------------------------------------------------------------------------*/
+
+    }
+}
---
+++ Ateliers.Core/Interfaces/IRegistInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ateliers
+{
+    /// <summary>
+    /// 登録情報
+    /// </summary>
+    /// <remarks>
+    /// <para> 概要: エンティティのデータ登録が行われた際の情報を保持していることを示す。 </para>
+    /// </remarks>
+    public interface IRegistInfo
+    {
+        /*--- Property/Field Definitions ----------------------------------------------------------------------------------------------------------*/
+
+        /// <summary> 
+        /// データ登録プログラムの識別文字列を取得または設定します。
+        /// </summary>
+        string RegistProgram { get; set; }
+
+        /// <summary> 
+        /// データ登録ユーザーの識別文字列を取得または設定します。
+        /// </summary>
+        string RegistUser { get; set; }
+
+        /// <summary> 
+        /// データ登録日時を取得または設定します。
+        /// </summary>
+        DateTime? RegistDateTime { get; set; }
+
+        /// <summary>
+        /// データ登録の状態を示す値を取得します。
+        /// </summary>
+        bool IsEnteredRegist { get; }
+
+        /*--- Method: public ----------------------------------------------------------------------------------------------------------------------*/
+
+    }
+}
